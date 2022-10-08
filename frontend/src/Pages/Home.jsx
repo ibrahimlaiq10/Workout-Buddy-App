b4079fe5c18a8216f1838cd4a04a3b9d9a011f7b
ibrahim <ibrahim1001.sc@gmail.com>
2022-10-08 11:03:33 +0500

    get workouts from api using  axios

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import WorkoutDetails from "../components/WorkoutDetails";
 
 function Home() {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    const getWorkouts = async () => {
+      try {
+        const res = await axios.get("http://localhost:4500/api/workout/");
+        const data = res.data;
+        setWorkouts(data);
+
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    getWorkouts();
+  }, []);
+
   return (
-    <h1>@Israr Home</h1>
-  )
+    <div className="home">
+    <div className="workouts">
+      {workouts && workouts.map(workout => (
+        <WorkoutDetails workout={workout} key={workout._id} />
+      ))}
+    </div>
+  </div>
+  );
 }
 
-export default Home
+export default Home;
